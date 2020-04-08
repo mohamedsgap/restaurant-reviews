88ed783df4b09eecd0e4b828b30e074fe0292dfb
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-04-09 01:43:16 +0200

    Update App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header'
+import  RestaurantsMap  from "./components/RestaurantsMap";
 
 function App() {
   return (
     <div className="App">
         <Header />
+        <RestaurantsMap />
     </div>
   );
 }
