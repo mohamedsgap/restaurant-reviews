c90af139470b5017d8887596c453fea7820da0d4
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-04-08 17:21:56 +0200

    Init RestaurantsList component

---
+++ src/components/RestaurantsList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './RestaurantsList.css'
+
+function RestaurantsList() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default RestaurantsList
