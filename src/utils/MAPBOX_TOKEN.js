7f9f84cc1cce3f1a80cd6565a31bf54406a7ce6e
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-04-09 01:38:27 +0200

    Add Mapbox token

---
+++ src/utils/MAPBOX_TOKEN.js
@@ -0,0 +1,3 @@
+// export const MAPBOX_TOKEN = 'sk.eyJ1IjoibW9oYW1lZHNnYXAiLCJhIjoiY2s4cmhwMXlsMDJ3ajNtcWJlbm9lanB3bSJ9.1MEWKFto5anyQf_8N-lDIg'; 
+
+export const MAPBOX_TOKEN = 'pk.eyJ1IjoibW9oYW1lZHNnYXAiLCJhIjoiY2p2NWNpc2liMHJ1MzN5cDJ3dmFsbTI1aSJ9.s59PBCspitI9M_EHQ0hgeg'
