8b580bcbd53d1d556966de93effd969f5f75f9c0
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-04-08 17:22:36 +0200

    Init RestaurantsMAp component

---
+++ src/components/RestaurantsMap.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './RestaurantsMap.css'
+
+function RestaurantsMap() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default RestaurantsMap
