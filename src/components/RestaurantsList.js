40ecefe8553692c33a497e90794cb1fa2dd7f830
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-04-09 01:41:55 +0200

    Render Map on page

--- src/components/RestaurantsList.js
+++ src/components/RestaurantsList.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import './RestaurantsList.css'
 
+
+
 function RestaurantsList() {
+    
     return (
         <div>
             
--- src/components/RestaurantsMap.js
+++ src/components/RestaurantsMap.js
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import MapGL, { GeolocateControl } from 'react-map-gl'
+import { MAPBOX_TOKEN } from "../utils/MAPBOX_TOKEN";
+import 'mapbox-gl/dist/mapbox-gl.css'
 import './RestaurantsMap.css'
 
+const TOKEN = MAPBOX_TOKEN
+
+const geolocateStyle = {
+    float: 'left',
+    margin: '50px',
+    padding: '10px'
+  }
+
 function RestaurantsMap() {
+    const [viewport, setViewPort] = useState({
+        width: "100%",
+        height: 880,
+        latitude: 0,
+        longitude: 0,
+        zoom: 2
+    })
+
+    const _onViewportChange = viewport => setViewPort({...viewport, transitionDuration: 3000 })
+
     return (
-        <div>
-            
+        <div style={{ margin: '0 auto'}}>  
+        <MapGL
+          {...viewport}
+          mapboxApiAccessToken={TOKEN}
+          mapStyle="mapbox://styles/mapbox/dark-v8"
+          onViewportChange={_onViewportChange}
+        >
+        <GeolocateControl
+          style={geolocateStyle}
+          positionOptions={{enableHighAccuracy: true}}
+          trackUserLocation={true}
+        />    
+        </MapGL>
         </div>
     )
 }
