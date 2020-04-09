521d24880decf9cc6da80d37001de5a64d0f44d2
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-04-09 15:27:59 +0200

    Render RestaurantsList components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header'
 import  RestaurantsMap  from "./components/RestaurantsMap";
+import  RestaurantsList  from "./components/RestaurantsList";
+
 
 function App() {
   return (
     <div className="App">
         <Header />
         <RestaurantsMap />
+        <RestaurantsList />
     </div>
   );
 }
