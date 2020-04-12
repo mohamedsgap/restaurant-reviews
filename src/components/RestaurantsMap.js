4809cc27f71a31c68fc8b323426c7d03990b7a8a
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-04-13 00:21:07 +0200

    Add user position marker

--- src/components/RestaurantsMap.js
+++ src/components/RestaurantsMap.js
@@ -1,24 +1,19 @@
 import React, { useState } from 'react'
-import MapGL, { GeolocateControl } from 'react-map-gl'
+import MapGL, {Marker} from 'react-map-gl'
 import { MAPBOX_TOKEN } from "../utils/MAPBOX_TOKEN";
 import 'mapbox-gl/dist/mapbox-gl.css'
 import './RestaurantsMap.css'
+import user_position_marker from '../images/user-position-marker.png'
 
 const TOKEN = MAPBOX_TOKEN
-
-const geolocateStyle = {
-    float: 'left',
-    margin: '50px',
-    padding: '10px'
-  }
 
 function RestaurantsMap() {
     const [viewport, setViewPort] = useState({
         width: "100%",
         height: 880,
-        latitude: 0,
-        longitude: 0,
-        zoom: 2
+        latitude: 30.822530,
+        longitude: 30.819080,
+        zoom: 10
     })
 
     const _onViewportChange = viewport => setViewPort({...viewport, transitionDuration: 3000 })
@@ -31,14 +26,36 @@ function RestaurantsMap() {
           mapStyle="mapbox://styles/mapbox/dark-v8"
           onViewportChange={_onViewportChange}
         >
-        <GeolocateControl
-          style={geolocateStyle}
-          positionOptions={{enableHighAccuracy: true}}
-          trackUserLocation={true}
-        />    
-        </MapGL>
-        </div>
+        <Marker
+        latitude={30.822530}
+        longitude={30.819080}
+        >
+          <button className="user-marker">
+            <img src={user_position_marker} alt="user-position-marker" />
+          </button>
+        </Marker>  
+      </MapGL>
+      </div>
     )
 }
 
 export default RestaurantsMap
+
+/*
+
+// GeoLocator component 
+ <GeolocateControl
+          style={geolocateStyle}
+          positionOptions={{enableHighAccuracy: true}}
+          trackUserLocation={true}
+  />
+
+*/
+
+/*
+const geolocateStyle = {
+    float: 'left',
+    margin: '50px',
+    padding: '10px'
+  }
+*/
