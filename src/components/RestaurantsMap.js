ef5ab358c98220c7b45d5c29c2c48833c90f06d6
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-04-14 00:27:12 +0200

    Set zoom viewport

--- src/components/RestaurantsMap.js
+++ src/components/RestaurantsMap.js
@@ -1,61 +1,60 @@
-import React, { useState } from 'react'
-import MapGL, {Marker} from 'react-map-gl'
+import React, { useState } from "react";
+import MapGL, { Marker } from "react-map-gl";
 import { MAPBOX_TOKEN } from "../utils/MAPBOX_TOKEN";
-import 'mapbox-gl/dist/mapbox-gl.css'
-import './RestaurantsMap.css'
-import user_position_marker from '../images/user-position-marker.png'
-import restaurant_position_marker from '../images/restaurant-position-marker.png'
+import "mapbox-gl/dist/mapbox-gl.css";
+import "./RestaurantsMap.css";
+import user_position_marker from "../images/user-position-marker.png";
+import restaurant_position_marker from "../images/restaurant-position-marker.png";
 
-const TOKEN = MAPBOX_TOKEN
+const TOKEN = MAPBOX_TOKEN;
 
 function RestaurantsMap(props) {
   const restaurantsData = props.data;
-  
-    const [viewport, setViewPort] = useState({
-        width: "100%",
-        height: 880,
-        latitude: 30.822530,
-        longitude: 30.819080,
-        zoom: 15
-    })
 
-    const _onViewportChange = viewport => setViewPort({...viewport, transitionDuration: 3000 })
+  const [viewport, setViewPort] = useState({
+    width: "100%",
+    height: 880,
+    latitude: 30.82253,
+    longitude: 30.81908,
+    zoom: 16
+  });
 
-    return (
-        <div className="map">  
-        <MapGL
-          {...viewport}
-          mapboxApiAccessToken={TOKEN}
-          mapStyle="mapbox://styles/mapbox/dark-v8"
-          onViewportChange={_onViewportChange}
-        >
-        <Marker
-        latitude={30.822530}
-        longitude={30.819080}
-        >
+  const _onViewportChange = viewport =>
+    setViewPort({ ...viewport, transitionDuration: 3000 });
+
+  return (
+    <div className="map">
+      <MapGL
+        {...viewport}
+        mapboxApiAccessToken={TOKEN}
+        mapStyle="mapbox://styles/mapbox/dark-v8"
+        onViewportChange={_onViewportChange}
+      >
+        <Marker latitude={30.82253} longitude={30.81908}>
           <button className="marker">
             <img src={user_position_marker} alt="user-position-marker" />
           </button>
         </Marker>
-        {
-         restaurantsData.map(restaurant =>  (
+        {restaurantsData.map(restaurant => (
           <Marker
-          key={restaurant.id}
-          latitude={restaurant.lat}
-          longitude={restaurant.long}
+            key={restaurant.id}
+            latitude={restaurant.lat}
+            longitude={restaurant.long}
           >
             <button className="marker">
-              <img src={restaurant_position_marker} alt="user-position-marker" />
+              <img
+                src={restaurant_position_marker}
+                alt="user-position-marker"
+              />
             </button>
-            </Marker>
-          )) 
-        }  
+          </Marker>
+        ))}
       </MapGL>
-      </div>
-    )
+    </div>
+  );
 }
 
-export default RestaurantsMap
+export default RestaurantsMap;
 
 /*
 
