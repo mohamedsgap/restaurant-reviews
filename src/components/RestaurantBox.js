86ec27c09dbed62e3bf91a6352f3f6b5a09e27b7
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-04-27 00:02:15 +0200

    Update RestaurantBox

--- src/components/RestaurantBox.js
+++ src/components/RestaurantBox.js
@@ -1,33 +1,44 @@
-import React from 'react'
-import './RestaurantBox.css'
+import React from "react";
+import Rater from "react-rater";
+import "react-rater/lib/react-rater.css";
+import "./RestaurantBox.css";
 
 function RestaurantBox(data) {
+  const boxData = data.data;
 
-    const boxData = data.data
-
-    const restaurantList = boxData.map(restaurant => {
-        return (
-          <div key={restaurant.id} className="section">
-            <h3>{restaurant.restaurantName}</h3>
-            <div>
-              {restaurant.ratings.map(restRate => {
-                return (
-                  <ul key={Math.random() * 100}>
-                    <li>Rating: {restRate.stars} stars</li>
-                    <li>Feedback: {restRate.comment}</li>
-                  </ul>
-                );
-              })}
-            </div>
-          </div>
-        );
-      });
-
+  const restaurantBoxData = boxData.map(restaurant => {
     return (
-        <div className="sublist">
-            {restaurantList}
+      <div key={restaurant.id} className="section">
+        <h3 className="restaurant-title">{restaurant.restaurantName}</h3>
+        <div className="rest-box">
+          <img
+            className="box-image"
+            src={restaurant.image}
+            alt="restaurant-pic"
+          />
+          <div className="rate-section">
+            {restaurant.ratings.map(restRate => {
+              return (
+                <ul key={Math.random() * 100}>
+                  <li>
+                    {" "}
+                    <Rater
+                      rating={restRate.stars}
+                      total={5}
+                      interactive={false}
+                    />{" "}
+                  </li>
+                  <li>Feedback: {restRate.comment}</li>
+                </ul>
+              );
+            })}
+          </div>
         </div>
-    )
+      </div>
+    );
+  });
+
+  return <div className="sublist">{restaurantBoxData}</div>;
 }
 
-export default RestaurantBox
+export default RestaurantBox;
