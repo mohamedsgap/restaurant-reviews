8f258be430821ce8d787240dd7d30152281ef389
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-05-22 09:25:02 +0200

    Refactor RestaurntBox component

--- src/components/RestaurantBox.js
+++ src/components/RestaurantBox.js
@@ -3,24 +3,34 @@ import Rater from "react-rater";
 import "react-rater/lib/react-rater.css";
 import "./RestaurantBox.css";
 import AddReviews from "./AddReviews";
+import RestaurantImage from "./RestaurantImage";
 
 function RestaurantBox(props) {
   const boxData = props.data;
-  const boxPlaces = props.places;
+  const boxGplaces = props.gplaces;
   const [starRate, setStarRate] = useState(0);
 
-  const restaurantBoxPlaces = boxPlaces.map(place => {
-    //let imageURL = `${place.venue.categories[0].icon.prefix}${place.venue.categories[0].icon.suffix}`
+  const restaurantBoxGplaces = boxGplaces.map(restaurant => {
     return (
-      <div key={place.venue.id} className="section">
-        <h3 className="restaurant-title">{place.venue.name}</h3>
-        <h4>Address: {place.venue.location.address}</h4>
+      <div key={restaurant.id} className="section">
+        <h3 className="restaurant-title">{restaurant.name}</h3>
         <div className="rest-box">
-          <img
-            className="box-image"
-            src={place.venue.photos.items}
-            alt="restaurant-pic"
-          />
+          {/* <img className="box-image" src={restImg} alt="restaurant-pic" />*/}
+          {/*console.log(restaurant.photos[0].photo_reference)*/}
+          <RestaurantImage imageRef={restaurant.photos[0].photo_reference} />
+          <div className="rate-section">
+            <ul>
+              <li>
+                {" "}
+                <Rater
+                  rating={restaurant.rating}
+                  total={5}
+                  interactive={false}
+                />{" "}
+              </li>
+              <li>User Reatings Total: {restaurant.user_ratings_total}</li>
+            </ul>
+          </div>
         </div>
         <div className="add-reviews">
           <AddReviews />
@@ -99,6 +109,38 @@ function RestaurantBox(props) {
       </div>
     ));
 
+  const filteredGrestaurants = boxGplaces
+    .filter(restaurant => restaurant.rating === starRate)
+    .map(filteredGrestaurant => (
+      <div key={filteredGrestaurant.id} className="section">
+        <h3 className="restaurant-title">{filteredGrestaurant.name}</h3>
+        <div className="rest-box">
+          <RestaurantImage
+            imageRef={filteredGrestaurant.photos[0].photo_reference}
+          />
+
+          <div className="rate-section">
+            <ul>
+              <li>
+                {" "}
+                <Rater
+                  rating={filteredGrestaurant.rating}
+                  total={5}
+                  interactive={false}
+                />{" "}
+              </li>
+              <li>
+                User Ratings Total: {filteredGrestaurant.user_ratings_total}
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="add-reviews">
+          <AddReviews />
+        </div>
+      </div>
+    ));
+
   return (
     <React.Fragment>
       <div className="filter-restaurants">
@@ -118,9 +160,36 @@ function RestaurantBox(props) {
       </div>
       <div className="sublist">
         <div>{starRate === 0 ? restaurantBoxData : filteredRestaurants}</div>
-        <div>{restaurantBoxPlaces}</div>
+        <div>
+          {starRate === 0 ? restaurantBoxGplaces : filteredGrestaurants}
+        </div>
+
+        {/* <div>{restaurantBoxPlaces}</div>*/}
       </div>
     </React.Fragment>
   );
 }
 export default RestaurantBox;
+
+/*
+const restaurantBoxPlaces = boxPlaces.map(place => {
+  //let imageURL = `${place.venue.categories[0].icon.prefix}${place.venue.categories[0].icon.suffix}`
+  return (
+    <div key={place.venue.id} className="section">
+      <h3 className="restaurant-title">{place.venue.name}</h3>
+      <h4>Address: {place.venue.location.address}</h4>
+      <div className="rest-box">
+        <img
+          className="box-image"
+          src={place.venue.photos.items}
+          alt="restaurant-pic"
+        />
+      </div>
+      <div className="add-reviews">
+        <AddReviews />
+      </div>
+    </div>
+  );
+});
+
+*/
