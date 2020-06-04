83f339130940e012e204689f6035532a3d3183ab
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-06-04 22:02:21 +0200

    Clean up code

--- src/App.js
+++ src/App.js
@@ -5,36 +5,17 @@ import Header from "./components/Header";
 import data from "./utils/restaurantsInfo.json";
 import FilterRestaurants from "./components/FilterRestaurants";
 
+/*
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
 const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
+*/
 const GKEY_ID = process.env.REACT_APP_HELPER_KEY;
 
 // https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&keyword=cruise&key=YOUR_API_KEY
 
 function App() {
-  const [places, setPlaces] = useState([]);
   const [gplaces, setGplaces] = useState([]);
 
-  useEffect(() => {
-    const END_POINT = "https://api.foursquare.com/v2/venues/explore?";
-    const params = {
-      client_id: CLIENT_ID,
-      client_secret: CLIENT_SECRET,
-      v: "20180323",
-      ll: "30.82253, 30.81908",
-      near: "Tanta",
-      query: "food"
-    };
-    axios
-      .get(END_POINT + new URLSearchParams(params))
-      .then(res => {
-        setPlaces(res.data.response.groups[0].items);
-      })
-      .catch(err => {
-        console.log("ERROR HAS OCURED" + err);
-      });
-  }, []);
-  /*
   //  API call for G_Places
   useEffect(() => {
     axios
@@ -48,12 +29,12 @@ function App() {
         console.log("ERROR HAS OCURED" + err);
       });
   }, []);
-*/
+
   return (
     <div className="App">
       <Header />
       <div>
-        <FilterRestaurants data={data} places={places} gplaces={gplaces} />
+        <FilterRestaurants data={data} gplaces={gplaces} />
       </div>
     </div>
   );
--- src/components/ExtraRestaurants.js
+++ src/components/ExtraRestaurants.js
@@ -88,34 +88,3 @@ function ExtraRestaurants(props) {
 }
 
 export default ExtraRestaurants;
-
-/*
-
-// to access: venue.location.lat/lng
-//const endPoint = 'https://api.foursquare.com/v2/venues/explore'
-// old api keys
-const CLIENT_ID = 'CDQOSZY5CJHZS40WUJBMVMRONYYQKFRRSCGXGUDBGEZO0VAK'
-const CLIENT_SECRET = 'H1Q10JL14ZFLJGONGPOQHPV03AFKRZZHNU3QRF3MUKE1OOL2'
-// new api keys
-const CLIENT_ID = "SSC5IB0OXGTUVE2BZFOOXQKVDXSSYOKDVVHL4QZPLHB5UQKD";
-const CLIENT_SECRET = "DLWMODCEGNV0VXRDGR010N5F1WBWPNNTNIBH0S0PFG4Q4N2H";
-
- const END_POINT = "https://api.foursquare.com/v2/venues/explore?";
-    const params = {
-      client_id: CLIENT_ID,
-      client_secret: CLIENT_SECRET,
-      v: "20180323",
-      ll: "30.82253, 30.81908",
-      near: "Tanta",
-      query: "food"
-    };
-    axios
-      .get(END_POINT + new URLSearchParams(params))
-      .then(res => {
-        //console.log(res.data.response.groups[0].items)
-        setPlaces(res.data.response.groups[0].items);
-      })
-      .catch(err => {
-        console.log("ERROR HAS OCURED" + err);
-      });
-*/
