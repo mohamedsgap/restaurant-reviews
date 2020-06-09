2b8980df226d2bb02a4b9e2e72871234d78604f7
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-06-10 01:48:49 +0200

    Updates

---
+++ precache-manifest.59973086be4f2e57f4f8c0a97ea8475b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "476b438faa68e1b66c91460d644fadd8",
+    "url": "/restaurants-review-site/index.html"
+  },
+  {
+    "revision": "73da3e7c4dcf8c24a394",
+    "url": "/restaurants-review-site/static/css/2.d4c1ea23.chunk.css"
+  },
+  {
+    "revision": "57216dc7a1faa5e04d1e",
+    "url": "/restaurants-review-site/static/css/main.b9c1139b.chunk.css"
+  },
+  {
+    "revision": "73da3e7c4dcf8c24a394",
+    "url": "/restaurants-review-site/static/js/2.462a50dd.chunk.js"
+  },
+  {
+    "revision": "0fb0e672f1308cb5b7677cdb93fc42ab",
+    "url": "/restaurants-review-site/static/js/2.462a50dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57216dc7a1faa5e04d1e",
+    "url": "/restaurants-review-site/static/js/main.8554b0dc.chunk.js"
+  },
+  {
+    "revision": "c7d2469ee6c844151b4b",
+    "url": "/restaurants-review-site/static/js/runtime-main.696c203a.js"
+  },
+  {
+    "revision": "e6c189c554ef2452dee19746186f4026",
+    "url": "/restaurants-review-site/static/media/logo.e6c189c5.png"
+  },
+  {
+    "revision": "4c717ae93e10b83e9e6d12afef08cd6e",
+    "url": "/restaurants-review-site/static/media/restaurant-position-marker.4c717ae9.png"
+  },
+  {
+    "revision": "972bdb89bc265615e54266921b5a2c5f",
+    "url": "/restaurants-review-site/static/media/user-position-marker.972bdb89.png"
+  }
+]);
