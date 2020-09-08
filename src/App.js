f48ed421726dcc137a174eb12d650c817a103f52
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-09-08 02:57:54 +0200

    feat: get user lang&lat and render places around

--- src/App.js
+++ src/App.js
@@ -1,40 +1,57 @@
 import React, { useState, useEffect } from "react";
+import Restaurants from "./components/Restaurants";
+import Header from "./components/Header";
 import axios from "axios";
 import "./App.css";
-import Header from "./components/Header";
-import data from "./utils/restaurantsInfo.json";
-import FilterRestaurants from "./components/FilterRestaurants";
 
-/*
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
 const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
-*/
-const GKEY_ID = process.env.REACT_APP_HELPER_KEY;
-
-// https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&keyword=cruise&key=YOUR_API_KEY
 
 function App() {
-  const [gplaces, setGplaces] = useState([]);
+  const [places, setPlaces] = useState([]);
+  const [userLatitude, setUserLatitude] = useState(null);
+  const [userLongitude, setUserLogitude] = useState(null);
 
-  //  API call for G_Places
+  // Automatically detect user geolocation
+  //  Forursquare API call for Places in user's area!
   useEffect(() => {
-    axios
-      .get(
-        `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=30.7970511,30.9987288&radius=1500&type=restaurant&keyword=cruise&key=${GKEY_ID}`
-      )
-      .then(res => {
-        setGplaces(res.data.results);
-      })
-      .catch(err => {
-        console.log("ERROR HAS OCURED" + err);
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setUserLatitude(position.coords.latitude);
+        setUserLogitude(position.coords.longitude);
+        const END_POINT = "https://api.foursquare.com/v2/venues/explore?";
+        const params = {
+          client_id: CLIENT_ID,
+          client_secret: CLIENT_SECRET,
+          v: "20180323",
+          ll: `${position.coords.latitude}, ${position.coords.longitude}`,
+          query: "restaurant",
+        };
+        axios
+          .get(END_POINT + new URLSearchParams(params))
+          .then((res) => {
+            setPlaces(res.data.response.groups[0].items);
+          })
+          .catch((err) => {
+            console.log("ERROR HAS OCURED" + err);
+          });
       });
-  }, []);
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  }, [navigator.geolocation.getCurrentPosition]);
 
+  console.log(userLongitude, userLatitude);
+  console.log(places);
   return (
     <div className="App">
       <Header />
       <div>
-        <FilterRestaurants data={data} gplaces={gplaces} />
+        <Restaurants
+          places={places}
+          userLatitude={userLatitude}
+          userLongitude={userLongitude}
+        />
       </div>
     </div>
   );
