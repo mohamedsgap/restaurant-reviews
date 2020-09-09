2429916e9c289b322407d50e08ffbaba8fd1b4c2
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-09-10 00:46:57 +0200

    clean up and add restaurnt img

--- src/components/ExtraRestaurants.js
+++ src/components/ExtraRestaurants.js
@@ -1,85 +1,50 @@
 import React, { useState } from "react";
-import Rater from "react-rater";
 import { Marker, Popup } from "react-map-gl";
 import "./ExtraRestaurants.css";
 import restaurant_marker from "../images/restaurant-position-marker.png";
-import RestaurantImage from "./RestaurantImage";
+import restaurantLogo from "../images/restaurants-logo.jpg";
 
 function ExtraRestaurants(props) {
-  const restaurantPlaces = props.gplaces;
+  const restaurantPlaces = props.places;
   const [selectedRestaurant, setSelectedRestaurant] = useState(null);
-
-  const allMarkers = restaurantPlaces.map(place => (
-    <Marker
-      key={place.id}
-      latitude={place.geometry.location.lat}
-      longitude={place.geometry.location.lng}
-    >
-      <button
-        className="marker"
-        onClick={e => {
-          e.preventDefault();
-          setSelectedRestaurant(place);
-        }}
-      >
-        <img src={restaurant_marker} alt="restaurant marker" />
-      </button>
-    </Marker>
-  ));
-
-  const filteredMarkers = restaurantPlaces
-    .filter(
-      restaurant =>
-        (restaurant.rating >= props.fromStars) &
-        (restaurant.rating <= props.toStars)
-    )
-    .map(filteredRestaurant => (
-      <Marker
-        key={filteredRestaurant.id}
-        latitude={filteredRestaurant.geometry.location.lat}
-        longitude={filteredRestaurant.geometry.location.lng}
-      >
-        <button
-          className="marker"
-          onClick={e => {
-            e.preventDefault();
-            setSelectedRestaurant(filteredRestaurant);
-          }}
-        >
-          <img src={restaurant_marker} alt="restaurant marker" />
-        </button>
-      </Marker>
-    ));
 
   return (
     <div>
-      {(props.fromStars === 0) & (props.toStars === 0)
-        ? allMarkers
-        : filteredMarkers}
-
+      {restaurantPlaces.map((place) => (
+        <Marker
+          key={place.venue.id}
+          latitude={place.venue.location.lat}
+          longitude={place.venue.location.lng}
+        >
+          <button
+            className="marker"
+            onClick={(e) => {
+              e.preventDefault();
+              setSelectedRestaurant(place);
+            }}
+          >
+            <img src={restaurant_marker} alt="restaurant marker" />
+          </button>
+        </Marker>
+      ))}
       {selectedRestaurant ? (
         <Popup
-          latitude={selectedRestaurant.geometry.location.lat}
-          longitude={selectedRestaurant.geometry.location.lng}
+          latitude={selectedRestaurant.venue.location.lat}
+          longitude={selectedRestaurant.venue.location.lng}
           onClose={() => {
             setSelectedRestaurant(null);
           }}
         >
           <div>
-            <h3 className="restaurant-title">{selectedRestaurant.name}</h3>
-            <p className="rest-rate">
-              Review:{" "}
-              <Rater
-                rating={selectedRestaurant.rating}
-                total={5}
-                interactive={false}
-              />{" "}
-            </p>
-            <div className="restaurant-image">
-              <RestaurantImage
-                imageRef={selectedRestaurant.photos[0].photo_reference}
-              />
-            </div>
+            <h3 className="restaurant-title">
+              {selectedRestaurant.venue.name}
+            </h3>
+            <p>Address: {selectedRestaurant.venue.location.address}</p>
+            <img
+              className="restaurantLogo"
+              src={restaurantLogo}
+              alt="restaurnt logo"
+            />
           </div>
         </Popup>
       ) : null}
